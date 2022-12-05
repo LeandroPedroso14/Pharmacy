99866a44f3b0bfe2159d950952a9127a3a4c10a3
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-04 21:42:16 -0300

    adding media url

--- Pharmacy/settings.py
+++ Pharmacy/settings.py
@@ -110,6 +110,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'))
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
