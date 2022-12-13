213c9b4ddda46304d4e19ec45a9d5d5305c585b4
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-12 22:05:27 -0300

    defining static files

--- pharm/settings.py
+++ pharm/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,6 +105,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'))
+STATIC_ROOT = os.path.join('static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
