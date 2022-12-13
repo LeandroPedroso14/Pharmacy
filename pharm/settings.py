6850cca07e4a8ddb0417e44a8345513c187e8ee3
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-12 22:10:02 -0300

    defining url for media

--- pharm/settings.py
+++ pharm/settings.py
@@ -108,6 +108,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'))
 STATIC_ROOT = os.path.join('static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 
 # Default primary key field type
