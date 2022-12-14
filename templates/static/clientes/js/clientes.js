7291d4b10d52c2b7c7653b0655ff0ce4f5836e42
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-14 09:45:00 -0300

    add classes in js

---
+++ templates/static/clientes/js/clientes.js
@@ -0,0 +1,7 @@
+function add_tratamento(){
+
+    container = document.getElementById('form-tratamento')
+
+    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='tratamento' class='form-control' name='tratamento'> </div> <div> class='col-md'><input type='text' placeholder='Doença' class='form-control' name='Doença'></div><div class 'col-md'> <input type='number' placeholder='idade' class='form-control' name='idade'> </div> </div><"
+
+}
