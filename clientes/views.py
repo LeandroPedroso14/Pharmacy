eb7d14034e1796d85f85d12a5d12bc3395d4881e
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-15 11:19:15 -0300

    returning and redeeming

--- clientes/views.py
+++ clientes/views.py
@@ -18,11 +18,11 @@ def clientes(request):
         cliente = Cliente.objects.filter(cpf=cpf)
 
         if cliente.exists():
-            return HttpResponse('Cliente já existe')
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome':sobrenome, 'email': email})
 
         if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
-         return HttpResponse('Email inválido')
-         cliente = Cliente(
+           return render(request, 'clientes.html', {'nome': nome, 'sobrenome':sobrenome, 'cpf': cpf})
+           cliente = Cliente(
            nome =  nome,
            sobrenome = sobrenome,
            email = email,
