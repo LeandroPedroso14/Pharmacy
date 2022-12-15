d6be544075604f2329aed4210f2c38059183d67d
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-15 07:05:36 -0300

    changing text

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -2,6 +2,6 @@ function add_tratamento(){
 
     container = document.getElementById('form-tratamento')
 
-    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='tratamento' class='form-control' name='tratamento'> </div> <div> class='col-md'><input type='text' placeholder='Doença' class='form-control' name='Doença'></div><div class 'col-md'> <input type='number' placeholder='idade' class='form-control' name='idade'> </div> </div><"
+    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='tratamento' class='form-control' name='tratamento'> </div> <div> class='col-md'><input type='text' placeholder='Triagem' class='form-control' name='Triagem'></div><div class 'col-md'> <input type='number' placeholder='idade' class='form-control' name='idade'> </div> </div><"
 
 }
