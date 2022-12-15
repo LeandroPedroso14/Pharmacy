bb40a5628792e3199252931b3a502e53d19cbb82
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-15 07:53:36 -0300

    add inner html

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -4,6 +4,6 @@ function add_tratamento(){
 
     html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='tratamento' class='form-control' name='tratamento'> </div> <div class='col-md'> <input type='text' placeholder='Triagem' class='form-control' name='Triagem'></div><div class 'col-md'> <input type='number' placeholder='idade' class='form-control' name='idade'> </div> </div><"
 
-    container += html
+    container.innerHTML += html
 
 }
