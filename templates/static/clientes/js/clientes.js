96afebf253813bbec764366c5cc3e5cf13323363
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-15 13:06:46 -0300

    changing js

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -2,7 +2,7 @@ function add_tratamento(){
 
     container = document.getElementById('form-tratamento')
 
-    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='tratamento' class='form-control' name='tratamento'> </div> <div class='col-md'> <input type='text' placeholder='Triagem' class='form-control' name='Triagem'></div><div class 'col-md'> <input type='number' placeholder='idade' class='form-control' name='idade'> </div> </div><"
+    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='Tratamento' class='form-control' name='tratamento'> </div> <div class='col-md'> <input type='text' placeholder='Triagem' class='form-control' name='triagem'></div><div class 'col-md'> <input type='number' placeholder='Idade' class='form-control' name='idade'> </div> </div><"
 
     container.innerHTML += html
 
