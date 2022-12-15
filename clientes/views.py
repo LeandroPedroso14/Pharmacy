b4aefb5dbcdf977a4bd22a052427243db0e60189
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-15 08:12:48 -0300

    add name

--- clientes/views.py
+++ clientes/views.py
@@ -5,4 +5,9 @@ def clientes(request):
     if request.method == "GET":
        return render(request,  'clientes.html')
     elif request.method == "POST":
-        nome = request.POST.get()
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+
+
