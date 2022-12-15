8f4a751aef1a921583a321ce3d87310975d2c022
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-15 08:40:51 -0300

    creating model classes for clients

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    nome = models.CharField(max_length=12)
